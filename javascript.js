79cd129ca8bdce5b0b78aa5f4b13ee6a687423db
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-31 20:25:57 +0800

    prompt for human choice added

--- javascript.js
+++ javascript.js
@@ -2,9 +2,11 @@ function getComputerChoice() {
     let rock = 'rock';
     let paper = 'paper';
     let scissors = 'scissors';
+
     function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+        return Math.floor(Math.random() * max);
     }
+
     let randomizer = getRandomInt(3);
 
     if (randomizer === 0) {
@@ -18,4 +20,10 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    return prompt("Enter one of the following choices: rock, paper, or scissors", '');
+}
+
 console.log(getComputerChoice());
+
+console.log(getHumanChoice());
