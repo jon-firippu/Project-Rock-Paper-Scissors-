88914a6c962c85c8e62a054ef5168ed565aa3caa
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-31 20:21:34 +0800

    randomizer function added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("TEST");
+function getComputerChoice() {
+    let rock = 'rock';
+    let paper = 'paper';
+    let scissors = 'scissors';
+    function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+    }
+    let randomizer = getRandomInt(3);
+
+    if (randomizer === 0) {
+        return rock;
+    }
+    if (randomizer === 1) {
+        return paper;
+    }
+    if (randomizer === 2) {
+        return scissors;
+    }
+}
+
+console.log(getComputerChoice());
