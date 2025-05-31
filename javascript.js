5e1a960bc27d0e8d77f037e5fbd9d9655f8c4bdb
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-31 20:48:02 +0800

    playround function done i think i can optimize the if conditionals further but for now this will do

--- javascript.js
+++ javascript.js
@@ -24,6 +24,57 @@ function getHumanChoice() {
     return prompt("Enter one of the following choices: rock, paper, or scissors", '');
 }
 
-console.log(getComputerChoice());
+let humanScore = 0;
 
-console.log(getHumanChoice());
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice.toLowerCase() === 'rock') {
+        if(computerChoice === 'rock') {
+            console.log("DRAW!");
+        }
+        if(computerChoice === 'paper') {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        }
+        if(computerChoice === 'scissors') {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    }
+
+    if (humanChoice.toLowerCase() === 'paper') {
+        if(computerChoice === 'paper') {
+            console.log("DRAW!");
+        }
+        if(computerChoice === 'scissors') {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        }
+        if(computerChoice === 'rock') {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        }
+    }
+
+    if (humanChoice.toLowerCase() === 'scissors') {
+        if(computerChoice === 'scissors') {
+            console.log("DRAW!");
+        }
+        if(computerChoice === 'rock') {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        }
+        if(computerChoice === 'paper') {
+            console.log("You win! Scissors beats Rock");
+            humanScore++;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
